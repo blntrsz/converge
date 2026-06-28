86415915b0d84dc0c7b7641bb234329edb44c6a3
Balint Orosz <orosz.balint.97@gmail.com>
2026-06-28 15:39:16 +0200

    feat(converge): add ReplicaSyncEngine service interface

--- packages/converge/src/sync-engine/index.ts
+++ packages/converge/src/sync-engine/index.ts
@@ -1,3 +1,4 @@
 export * as PrimarySyncEngine from "./services/primary-sync-engine";
+export * as ReplicaSyncEngine from "./services/replica-sync-engine";
 export * as IndexedDbPrimarySyncEngine from "./layers/indexeddb-primary-sync-engine";
 export * as PostgresPrimarySyncEngine from "./layers/postgres-primary-sync-engine";
